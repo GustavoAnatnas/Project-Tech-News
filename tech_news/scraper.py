bfdd4dd7a42cd28cc8b35aa9d07eb3fba7b30d98
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2023-02-09 20:04:28 -0300

    Cria a função scrape_news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,7 @@
 import pip._vendor.requests as requests
 import time
+
+# import re
 from parsel import Selector
 
 
@@ -20,20 +22,54 @@ def fetch(url):
 # Requisito 2
 def scrape_updates(html_content):
     sel = Selector(html_content)
-    urls = sel.css('a.cs-overlay-link::attr(href)').getall()
+    urls = sel.css("a.cs-overlay-link::attr(href)").getall()
     return urls
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
     sel = Selector(html_content)
-    next_page_link = sel.css('a.next.page-numbers::attr(href)').get()
+    next_page_link = sel.css("a.next.page-numbers::attr(href)").get()
     return next_page_link if next_page_link else None
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    sel = Selector(html_content)
+
+    return {
+        "url": sel.css("link[rel=canonical]::attr(href)").get(),
+        "title": sel.css("h1.entry-title::text").get().strip(),
+        "timestamp": sel.css("li.meta-date::text").get().strip(),
+        "writer": sel.css("span.author a::text").get().strip(),
+        "reading_time": int(
+            sel.css("li.meta-reading-time::text").get().strip().split()[0]
+        ),
+        "summary": sel.xpath("string(//p[1])").get().strip(),
+        "category": sel.css("div.meta-category span.label::text")
+        .get()
+        .strip(),
+    }
+
+    # selector = Selector(html_content)
+
+    # url = selector.css("link[rel=canonical]::attr(href)").get()
+    # title = selector.css(".entry-title::text").get().strip()
+    # timestamp = selector.css(".meta-date::text").get()
+    # writer = selector.css(".author a::text").get()
+    # reading_time = selector.css(".meta-reading-time::text").get()
+    # summary = selector.xpath("string(//p)").get().strip()
+    # category = selector.css(".meta-category span.label::text").get()
+
+    # return {
+    #     "url": url,
+    #     "title": title,
+    #     "timestamp": timestamp,
+    #     "writer": writer,
+    #     "reading_time": int("".join(re.findall(r"\d", reading_time))),
+    #     "summary": summary,
+    #     "category": category,
+    # }
 
 
 # Requisito 5
